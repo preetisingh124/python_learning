b15d7984c98857b736022c15171cb2b6d9f98753
preetisingh124 <preeti.singh@vamstar.io>
2021-05-14 23:41:48 +0530

    csv file uploaded

---
+++ csv.py
@@ -0,0 +1,11 @@
+import csv
+
+# opening the CSV file
+with open('ceo.csv', mode ='r')as file:
+	
+# reading the CSV file
+csvFile = csv.reader(file)
+
+# displaying the contents of the CSV file
+for lines in csvFile:
+		print(lines)
