87f7d6f587bb01e863c8c3b742ee466a9d2938bc
preetisingh124 <84151937+preetisingh124@users.noreply.github.com>
2021-05-17 14:36:11 +0530

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,11 +1,5 @@
 import csv
-
-# opening the CSV file
 with open('ceo.csv', mode ='r')as file:
-	
-# reading the CSV file
-csvFile = csv.reader(file)
-
-# displaying the contents of the CSV file
+   csvFile = csv.reader(file)
 for lines in csvFile:
 		print(lines)
